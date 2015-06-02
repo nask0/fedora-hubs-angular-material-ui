2a1bb018dd9661036b6ded4781557c7e8fa75e45
nask0 <arrtedone@gmail.com>
2015-06-03 00:53:45 +0300

    Integrated Ralph widgtes (https://github.com/ralphbean/fedora-hubs-prototype)

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -1,28 +1,33 @@
 'use strict';
 
 angular.module('fedoraHubs')
-.controller('HomeController', ['$scope', '$rootScope', '$mdSidenav', 'DemoHubs',
-    function( $scope, $rootScope, $mdSidenav, DemoHubs ) {
+.controller('HomeController', ['$scope', '$rootScope', '$mdSidenav', '$timeout', 'DemoHubs',
+    function( $scope, $rootScope, $mdSidenav, $timeout, DemoHubs ) {
         // $scope.openLeftMenu = function() { $mdSidenav('left').toggle(); };
 
         $scope.tabError = false;
         $scope.tabContent = false;
+        $scope.tabLoading = false;
 
-        $scope.fetchTabContent = function( tab ) {
-            switch( tab ) {
-                case 'subscribers':
-                    DemoHubs.getSubscribers().then(
-                        function( data ) {
-                            $scope.tabContent = data;
-                        },
-                        function( error ) {
-                            $scope.tabContent = 'Error occured while fetching data !';
-                            console.log(error);
-                        }
-                    )
-                break;
-            }
+        $scope.loadWidgetData  = function( widget ) {
+            $scope.tabLoading = true;
+            DemoHubs.loadWidgetData( widget ).then(
+                function( data ) {
+                    $timeout(function() {
+                        console.log('Response data:', data);
+                        $scope.tabContent = data;
+                        $scope.tabLoading = false;
+                    }, 650); // lag a little in order to show loading :)
 
-        }
+                },
+                function( error, httpStatus ) {
+                    $scope.tabContent = 'Error (' + httpStatus + ') occured while fetching widget data';
+                    console.log(error);
+                    $scope.tabLoading = false;
+                }
+            );
+        };
+
+        $scope.loadWidgetData( 1 );
     }
 ]);
--- js/services/DemoHubs.js
+++ js/services/DemoHubs.js
@@ -2,19 +2,50 @@
 /**
  * DemoHubs service
  * Fetches data from Ralph hubs prototype (https://github.com/ralphbean/fedora-hubs-prototype) and displays it on client.
+ *
+ * @todo:
+ *     - remove hardcoded url's
  */
 angular.module('fedoraHubs')
-    .factory('DemoHubs', [ '$http',
-        function( $http ) {
+    .factory('DemoHubs', [ '$http', '$q',
+        function( $http, $q ) {
             var DemoHubs= {};
 
-            DemoHubs.getSubscribers = function() {
-                return $http.get('http://localhost:5000/designteam/1/json/');
+            DemoHubs.loadWidgetData = function( widget ) {
+                var deferred = $q.defer();
+
+                $http.get('http://localhost:5000/designteam/' + parseInt(widget) + '/json/')
+                    // additional params: status, headers, config
+                    .success( function(data) {
+                        // this callback will be called asynchronously
+                        // when the response is available
+                        deferred.resolve(data);
+                    }).error( function(data, status) { // , headers, config
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                        deferred.reject(data, status);
+                    });
+
+                return deferred.promise;
             };
 
-            DemoHubs.getRulesAndOwners = function() {
-                return $http.get('http://localhost:5000/designteam/2/json/');
-            }
+            DemoHubs.getSubscribers = function() {
+                var deferred = $q.defer();
+
+                $http.get('http://localhost:5000/designteam/1/json/')
+                     .success(
+                        function(data) { // additional params: status, headers, config
+                            // this callback will be called asynchronously when the response is available
+                            deferred.resolve(data);
+                        }
+                    )
+                    .error(
+                        function(data, status) { // , headers, config
+                            // called asynchronously if an error occurs or server returns response with an error status.
+                            deferred.reject(data, status);
+                        }
+                    );
+            };
 
             return DemoHubs;
         }
