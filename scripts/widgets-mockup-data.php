3a46479ccaad63fd5524be00ef97725a0c18ac5d
nask0 <arrtedone@gmail.com>
2015-06-03 02:00:09 +0300

    ...

---
+++ scripts/widgets-mockup-data.php
@@ -0,0 +1,25 @@
+<?php
+$_widget = new stdClass();
+$_widget->id = mt_rand(1,999);
+$_widget->name = 'Subscribers';
+$_widget->desc = 'Widget description';
+
+$_widget_data = new stdClass();
+$_widget_data->members = mt_rand(1,99999);
+$_widget_data->subscribers = mt_rand(1000, 999999);
+$_widget_data->source_url = '/source/stats';
+
+$_widget_another_data = new stdClass();
+$_widget_data->downloads = mt_rand(1,99999);
+$_widget_data->updates = mt_rand(1000, 999999);
+$_widget_data->source_url = '/example';
+
+$widgetData = array(
+    'widget' => $_widget,
+    'data' => array(
+        $_widget_data,
+        $_widget_another_data
+    )
+);
+
+echo json_encode($widgetData);
