f18d63644dc6e1acefaee364bb1479c2d0410f7d
nask0 <arrtedone@gmail.com>
2015-06-05 04:48:39 +0300

    demo hub page

--- js/app.js
+++ js/app.js
@@ -54,18 +54,8 @@ angular.module('fedoraHubs', ['ui.router', 'ngMaterial'])
                     }
                 }
             })
-            // design
-            .state('index.design', {
-                url: "design"
-                /*views: {
-                 'content': {
-                    controller: 'ExampleController',
-                    templateUrl: window.appConfig.baseUrl + 'templates/example/example.html'
-                 }
-                }*/
-            })
-            .state('test', {
-                url: '/test',
+            .state('design', {
+                url: '/design',
                 views: {
                     'navigation': {
                         templateUrl: window.appConfig.baseUrl + 'templates/partials/navigation.html'
@@ -87,9 +77,37 @@ angular.module('fedoraHubs', ['ui.router', 'ngMaterial'])
          *
          * @todo: research how to define custom theme, it should be trivial
          * */
+
+        $mdThemingProvider.definePalette('fedoraHubs', {
+            '50': 'ffebee',
+            '100': 'ffcdd2',
+            '200': 'ef9a9a',
+            '300': 'e57373',
+            '400': 'ef5350',
+            '500': 'f44336',
+            '600': 'e53935',
+            '700': 'd32f2f',
+            '800': 'c62828',
+            '900': 'b71c1c',
+            'A100': 'ff8a80',
+            'A200': 'ff5252',
+            'A400': 'ff1744',
+            'A700': 'd50000',
+            'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                                // on this palette should be dark or light
+            'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+                '200', '300', '400', 'A100'],
+            'contrastLightColors': 'dark'    // could also specify this if default was 'dark'
+        })
+        .extendPalette('fedoraHubs', {
+                'hue-test': 'fefefe'
+            });
+
         $mdThemingProvider.theme('blue')
-            .primaryPalette('light-blue')
-            .accentPalette('teal');
+            .primaryPalette('fedoraHubs', {
+        })
+        ;
+        /*.accentPalette('light-blue');*/
     }
 ])
 /**
